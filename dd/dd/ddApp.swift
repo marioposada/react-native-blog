d1385305eaa075c920f5f40fe1be705f317b2f93
Mario Posada <marioposadamiranda@gmail.com>
2022-10-03 12:00:44 -0300

    First commit

---
+++ dd/dd/ddApp.swift
@@ -0,0 +1,17 @@
+//
+//  ddApp.swift
+//  dd
+//
+//  Created by Mario Posada on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
