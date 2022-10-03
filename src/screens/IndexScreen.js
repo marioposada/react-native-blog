691fb07913591b93b774aeb55f07ed9e69029829
Mario Posada <marioposadamiranda@gmail.com>
2022-10-03 13:34:07 -0300

    Add Context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {useContext} from 'react';
+import { StyleSheet, Text, View , FlatList} from 'react-native';
+
+import BlogContext from '../context/BlogContext';
 
 
 const IndexScreen = () => {
+const blogPosts = useContext(BlogContext);
+
   return (
     <View>
       <Text>Index Screen</Text>
+      <FlatList 
+      data={blogPosts}
+      keyExtractor={(blogPost) => blogPost.title}
+      renderItem={({item}) => {
+        return <Text>{item.title}</Text>
+      }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({});
+
 export default IndexScreen;
